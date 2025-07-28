a22c56b2ea3f0c74b10796b218703fe98a099f65
Panchenko Daria <pancenkodara181@gmail.com>
2025-07-28 16:51:27 +0700

    Delete lab7/build directory

--- lab7/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/darya/evm/lab7
-# Build directory: /home/darya/evm/lab7/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(InversionTest "/home/darya/evm/lab7/build/InversionTest")
-set_tests_properties(InversionTest PROPERTIES  _BACKTRACE_TRIPLES "/home/darya/evm/lab7/CMakeLists.txt;27;add_test;/home/darya/evm/lab7/CMakeLists.txt;0;")
